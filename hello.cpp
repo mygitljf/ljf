f34665dfe552c05293aee972d6ddb6e848137158
ljf <2410316423@qq.com>
2023-02-27 10:17:05 +0800

    ljf

---
+++ hello.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tom on 23-2-27.
+//
+
+#include "hello.h"
---
+++ hello.h
@@ -0,0 +1,14 @@
+//
+// Created by tom on 23-2-27.
+//
+
+#ifndef GITTEXT_HELLO_H
+#define GITTEXT_HELLO_H
+
+
+class hello {
+
+};
+
+
+#endif //GITTEXT_HELLO_H
